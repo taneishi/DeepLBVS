a31512af9d2d9d13f99017d0ec5d37836b04d59e
taneishi <taneishi@users.noreply.github.com>
2015-02-27 10:58:07 +0900

    remove DATASET

--- DBN.py
+++ DBN.py
@@ -10,7 +10,7 @@ import theano
 import theano.tensor as T
 from theano.tensor.shared_randomstreams import RandomStreams
 
-from utils import load_data, DATASET
+from utils import load_data
 
 from code.logistic_sgd import LogisticRegression 
 from code.mlp import HiddenLayer
@@ -232,7 +232,6 @@ def test_DBN(finetune_lr=0.1, pretraining_epochs=100,
 
 
 if __name__ == '__main__':
-    dataset = DATASET
     if len(sys.argv) > 3:
         dataset = sys.argv[1]
         n_units = int(sys.argv[2])
