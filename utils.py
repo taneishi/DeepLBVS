d7361d79c42d5c17085f2cabe97cd0fd0a0ec8f1
taneishi <taneishi@users.noreply.github.com>
2015-11-06 18:22:50 +0900

    update for .npz

--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
-"""
-utils for custom dataset
-"""
+# utils for custom dataset
 import os
 import sys
 import pandas as pd
@@ -18,24 +16,15 @@ def build(dataset):
     import gzip,cPickle
     filename = dataset + '.pkl.gz'
     data = cPickle.load(gzip.open(filename))
-    cat = []
+    label = []
     for row in data:
         df = pd.DataFrame(row[0])
         df['label'] = row[1]
-        cat.append(df)
-    df = pd.concat(cat)
+        label.append(df)
+    df = pd.concat(label)
     df.to_pickle(dataset)
 
 def result(classifier, y):
-    """Return a float representing the number of errors in the minibatch
-    over the total number of examples of the minibatch ; zero one
-    loss over the size of the minibatch
-
-    :type y: theano.tensor.TensorType
-    :param y: corresponds to a vector that gives for each example the
-              correct label
-    """
-
     # check if y has same dimension of y_pred
     if y.ndim != classifier.y_pred.ndim:
         raise TypeError(
@@ -51,12 +40,6 @@ def result(classifier, y):
         raise NotImplementedError()
 
 def load_data(dataset, nfold=5):
-    ''' Loads the dataset
-
-    :type dataset: string
-    :param dataset: the path to the dataset (here MNIST)
-    '''
-
     #############
     # LOAD DATA #
     #############
@@ -64,35 +47,12 @@ def load_data(dataset, nfold=5):
     print '... loading data'
 
     # Load the dataset
-    if dataset.endswith('npz'):
-        df = numpy.load(dataset)['mat']
-    else:
-        df = pd.read_pickle(dataset)
-        if os.path.basename(dataset) != 'mnist':
-            numpy.random.seed(123)
-            df = df.reindex(numpy.random.permutation(df.index))
-        df = df.values
-
-    if os.path.basename(dataset) != 'mnist':
-        # purge invariance
-        df = df[:, df.max(axis=0) != df.min(axis=0)]
-        # scaling [0,1]
-        df[:,:-1] = (df[:,:-1] - df[:,:-1].min(axis=0)) / (df[:,:-1].max(axis=0) - df[:,:-1].min(axis=0))
+    df = numpy.load(dataset)['data']
 
     train_set = df[:-df.shape[0] / nfold]
     test_set = df[-df.shape[0] / nfold:]
 
-    #train_set, test_set format: pandas.DataFrame(columns=[0,1,2,...,label])
-
     def shared_dataset(data_xy, borrow=True):
-        """ Function that loads the dataset into shared variables
-
-        The reason we store our dataset in shared variables is to allow
-        Theano to copy it into the GPU memory (when code is run on GPU).
-        Since copying data into the GPU is slow, copying a minibatch everytime
-        is needed (the default behaviour if the data is not in a shared
-        variable) would lead to a large decrease in performance.
-        """
         data_x = data_xy[:,:-1]
         data_y = data_xy[:,-1]
         shared_x = theano.shared(numpy.asarray(data_x,
@@ -101,13 +61,6 @@ def load_data(dataset, nfold=5):
         shared_y = theano.shared(numpy.asarray(data_y,
                                                dtype=theano.config.floatX),
                                  borrow=borrow)
-        # When storing data on the GPU it has to be stored as floats
-        # therefore we will store the labels as ``floatX`` as well
-        # (``shared_y`` does exactly that). But during our computations
-        # we need them as ints (we use labels as index, and if they are
-        # floats it doesn't make sense) therefore instead of returning
-        # ``shared_y`` we will have to cast it to int. This little hack
-        # lets ous get around this issue
         return shared_x, T.cast(shared_y, 'int32')
 
     test_set_x, test_set_y = shared_dataset(test_set)
@@ -115,135 +68,6 @@ def load_data(dataset, nfold=5):
 
     rval = [(train_set_x, train_set_y), (test_set_x, test_set_y)]
     return rval
-
-
-def scale_to_unit_interval(ndar, eps=1e-8):
-    """ Scales all values in the ndarray ndar to be between 0 and 1 """
-    ndar = ndar.copy()
-    ndar -= ndar.min()
-    ndar *= 1.0 / (ndar.max() + eps)
-    return ndar
-
-
-def tile_raster_images(X, img_shape, tile_shape, tile_spacing=(0, 0),
-                       scale_rows_to_unit_interval=True,
-                       output_pixel_vals=True):
-    """
-    Transform an array with one flattened image per row, into an array in
-    which images are reshaped and layed out like tiles on a floor.
-
-    This function is useful for visualizing datasets whose rows are images,
-    and also columns of matrices for transforming those rows
-    (such as the first layer of a neural net).
-
-    :type X: a 2-D ndarray or a tuple of 4 channels, elements of which can
-    be 2-D ndarrays or None;
-    :param X: a 2-D array in which every row is a flattened image.
-
-    :type img_shape: tuple; (height, width)
-    :param img_shape: the original shape of each image
-
-    :type tile_shape: tuple; (rows, cols)
-    :param tile_shape: the number of images to tile (rows, cols)
-
-    :param output_pixel_vals: if output should be pixel values (i.e. int8
-    values) or floats
-
-    :param scale_rows_to_unit_interval: if the values need to be scaled before
-    being plotted to [0,1] or not
-
-
-    :returns: array suitable for viewing as an image.
-    (See:`Image.fromarray`.)
-    :rtype: a 2-d array with same dtype as X.
-
-    """
-
-    assert len(img_shape) == 2
-    assert len(tile_shape) == 2
-    assert len(tile_spacing) == 2
-
-    # The expression below can be re-written in a more C style as
-    # follows :
-    #
-    # out_shape    = [0,0]
-    # out_shape[0] = (img_shape[0]+tile_spacing[0])*tile_shape[0] -
-    #                tile_spacing[0]
-    # out_shape[1] = (img_shape[1]+tile_spacing[1])*tile_shape[1] -
-    #                tile_spacing[1]
-    out_shape = [
-        (ishp + tsp) * tshp - tsp
-        for ishp, tshp, tsp in zip(img_shape, tile_shape, tile_spacing)
-    ]
-
-    if isinstance(X, tuple):
-        assert len(X) == 4
-        # Create an output numpy ndarray to store the image
-        if output_pixel_vals:
-            out_array = numpy.zeros((out_shape[0], out_shape[1], 4),
-                                    dtype='uint8')
-        else:
-            out_array = numpy.zeros((out_shape[0], out_shape[1], 4),
-                                    dtype=X.dtype)
-
-        #colors default to 0, alpha defaults to 1 (opaque)
-        if output_pixel_vals:
-            channel_defaults = [0, 0, 0, 255]
-        else:
-            channel_defaults = [0., 0., 0., 1.]
-
-        for i in xrange(4):
-            if X[i] is None:
-                # if channel is None, fill it with zeros of the correct
-                # dtype
-                dt = out_array.dtype
-                if output_pixel_vals:
-                    dt = 'uint8'
-                out_array[:, :, i] = numpy.zeros(
-                    out_shape,
-                    dtype=dt
-                ) + channel_defaults[i]
-            else:
-                # use a recurrent call to compute the channel and store it
-                # in the output
-                out_array[:, :, i] = tile_raster_images(
-                    X[i], img_shape, tile_shape, tile_spacing,
-                    scale_rows_to_unit_interval, output_pixel_vals)
-        return out_array
-
-    else:
-        # if we are dealing with only one channel
-        H, W = img_shape
-        Hs, Ws = tile_spacing
-
-        # generate a matrix to store the output
-        dt = X.dtype
-        if output_pixel_vals:
-            dt = 'uint8'
-        out_array = numpy.zeros(out_shape, dtype=dt)
-
-        for tile_row in xrange(tile_shape[0]):
-            for tile_col in xrange(tile_shape[1]):
-                if tile_row * tile_shape[1] + tile_col < X.shape[0]:
-                    this_x = X[tile_row * tile_shape[1] + tile_col]
-                    if scale_rows_to_unit_interval:
-                        # if we should scale values to be between 0 and 1
-                        # do this by calling the `scale_to_unit_interval`
-                        # function
-                        this_img = scale_to_unit_interval(
-                            numpy.resize(this_x, img_shape))
-                    else:
-                        this_img = numpy.resize(this_x, img_shape)
-                    # add the slice to the corresponding position in the
-                    # output array
-                    c = 1
-                    if output_pixel_vals:
-                        c = 255
-                    out_array[
-                        tile_row * (H + Hs): tile_row * (H + Hs) + H,
-                        tile_col * (W + Ws): tile_col * (W + Ws) + W
-                    ] = this_img * c
-        return out_array
 
 if __name__ == '__main__':
     if not os.path.exists(DATASET):
