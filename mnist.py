b2330b9cee50388ad9bfd3487c115471f19c42c8
taneishi <taneishi@users.noreply.github.com>
2016-01-07 15:40:57 +0900

    append svmlight and npz

--- mnist.py
+++ mnist.py
@@ -2,8 +2,9 @@
 import pandas as pd
 import numpy as np
 import gzip,cPickle
+import itertools
 
-def main():
+def mnist():
     filename = '../deep/data/mnist.pkl.gz'
     data = cPickle.load(gzip.open(filename))
     mat = []
@@ -13,6 +14,23 @@ def main():
         mat.append(df)
     data = pd.concat(mat)
     np.savez('data/mnist.npz', data=data.values)
+
+def npz():
+    out = open('out1.train', 'w')
+    data = np.load('data/out1.npz')['data']
+    for target,row in itertools.izip(data[:,-1], data[:,:-1]):
+        row = map(lambda x: '%d:%f' % (x[0],x[1]), enumerate(row, 1))
+        out.write('%d %s\n' % (target, ' '.join(row)))
+    out.close()
+
+def libsvm():
+    out = open('out1.train', 'w')
+    df = pd.read_pickle('../gpcr/pkl/out1')
+    for i,row in df.iterrows():
+        target = row[-1]
+        row = map(lambda x: '%d:%f' % (x[0],x[1]), enumerate(row.values[:-1], 1))
+        out.write('%d %s\n' % (target, ' '.join(row)))
+    out.close()
 
 if __name__ == '__main__':
     main()
