1ea42a9d002edf42b8dca8b85552a80816ade1fc
taneishi <taneishi@users.noreply.github.com>
2017-10-31 17:12:28 +0900

    re-import keras DNN model
    Former-commit-id: 17349451434fd8578e45e7c61f1db461b66e017c
    Former-commit-id: 50ed62bab766fc736c68223c0e85456a0f6b66ca
    Former-commit-id: dfe85cfe787e50142b0415167ce9d25785f7d989

---
+++ main.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import sys
+import os
+import dnn
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        datafile = sys.argv[1]
+    else:
+        sys.exit('Usage: %s [datafile]' % (sys.argv[0]))
+
+    for dirname in ['model','result']:
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
+
+    np.random.seed(123)
+
+    dnn.setup()
+    dnn.show_version()
+
+    print('Data loading ...')
+    data = np.load(datafile)['data']
+    np.random.shuffle(data)
+    print(str(data.shape))
+
+    taskname = os.path.basename(datafile)
+    optimizer = 'Adam'
+    lr = 0.0001
+    nb_epoch = 1000
+    dropout = 0.1
+
+    for activation in ['sigmoid']:
+        for batch_size in [1500]:
+            for unit1 in [3000]:
+                for unit2 in [60]:
+                    dnn.validation(taskname, data, layers=[unit1, unit2], 
+                            batch_size=batch_size, nb_epoch=nb_epoch, class_weight=None,
+                            optimizer=optimizer, lr=lr, activation=activation,
+                            dropout=dropout, patience=100, count=1)
