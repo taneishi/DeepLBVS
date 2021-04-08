14eb79fafa47d5fe9ee26d50b112cf0bd0568077
taneishi <taneishi@users.noreply.github.com>
2021-04-08 09:56:57 +0900

    renamed data directory.

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
 def main():
-    gpcr = pd.read_csv('data/gpcr.txt', sep='\t')
-    gpcr_fasta = pd.read_csv('data/gpcr_fasta.txt', sep='\t')
+    gpcr = pd.read_csv('data/glida/gpcr.txt', sep='\t')
+    gpcr_fasta = pd.read_csv('data/glida/gpcr_fasta.txt', sep='\t')
 
     gpcr = pd.merge(gpcr, gpcr_fasta, on='name', how='left')
     print(gpcr)
 
-    ligand = pd.read_csv('data/ligand.txt', sep='\t')
-    ligand_smi = pd.read_csv('data/ligand_smi.txt', sep='\t')
+    ligand = pd.read_csv('data/glida/ligand.txt', sep='\t')
+    ligand_smi = pd.read_csv('data/glida/ligand_smi.txt', sep='\t')
 
     ligand = pd.merge(ligand, ligand_smi, on='name', how='left')
     print(ligand)
 
-    relation = pd.read_csv('data/relation.txt', sep='\t')
+    relation = pd.read_csv('data/glida/relation.txt', sep='\t')
 
     relation = pd.merge(relation[['ligand_id', 'gpcr_id', 'activity']], gpcr[['gpcr_id', 'fasta']], on='gpcr_id', how='left')
     relation = pd.merge(relation, ligand[['ligand_id', 'smi']], on='ligand_id', how='left')
