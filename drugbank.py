b916a8fe783f059c63bdd5264972dfc7588907e9
taneishi <taneishi@users.noreply.github.com>
2021-05-14 23:03:07 +0900

    used forwardmolsupplier for gzipped sdf.

--- drugbank.py
+++ drugbank.py
@@ -1,16 +1,20 @@
 import pandas as pd
 from rdkit import Chem
+import gzip
 
 def main():
+    input_dir = '../data/drugbank'
+    output_dir = 'data'
+
     drugbank = []
-    for mol in Chem.SDMolSupplier('data/drugbank.sdf'):
+    for mol in Chem.ForwardSDMolSupplier(gzip.open('%s/drugbank.sdf.gz' % (input_dir))):
         if mol:
             drugbank_id = mol.GetProp('DRUGBANK_ID')
             drugbank.append([drugbank_id, Chem.MolToSmiles(mol)])
 
     drugbank = pd.DataFrame(drugbank)
     drugbank.columns = ['drugbank_id', 'smi']
-    drugbank.to_csv('data/drugbank.tsv', sep='\t', index=False)
+    drugbank.to_csv('%s/drugbank.tsv' % (output_dir), sep='\t', index=False)
     print(drugbank)
 
 if __name__ == '__main__':
