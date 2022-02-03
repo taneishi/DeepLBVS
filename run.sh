661c2052a51946cd94cb3ef3af395b42f8e2acc9
taneishi <taneishi@users.noreply.github.com>
2022-02-03 16:06:38 +0900

    appended run script for ecfp generation and rf prediction.

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -d torch ]; then
+    source torch/bin/activate
+else
+    python3 -m venv torch
+    source torch/bin/activate
+    pip install --upgrade pip
+    pip install torch numpy scikit-learn pandas rdkit-pypi
+fi
+
+python pcba.py --sort --limit 10
