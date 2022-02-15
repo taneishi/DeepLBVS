a4e9b1f2c9319aa361f0ea601d7c6c2e607f0637
taneishi <taneishi@users.noreply.github.com>
2022-02-15 10:03:07 +0900

    appended xgb.py.

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ else
     python3 -m venv torch
     source torch/bin/activate
     pip install --upgrade pip
-    pip install torch numpy scikit-learn pandas rdkit-pypi
+    pip install torch numpy scikit-learn xgboost pandas rdkit-pypi
 fi
 
 wget -c -P data https://github.com/deepchem/deepchem/raw/master/datasets/pcba.csv.gz
